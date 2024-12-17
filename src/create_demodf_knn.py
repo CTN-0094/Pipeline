d330362b2b9d22054fe91e3219bb387722a802d6
AaronMarker <aaronmarker@comcast.net>
2024-12-17 12:09:32 -0500

    Added functionality to download MatchIt package if not installed

--- src/create_demodf_knn.py
+++ src/create_demodf_knn.py
@@ -1,10 +1,10 @@
 import pandas as pd
 from psmpy import PsmPy
 import rpy2.robjects as robjects
-from rpy2.robjects.packages import importr
+from rpy2.robjects.packages import importr, PackageNotInstalledError
 import pandas as pd
 from rpy2.robjects.conversion import localconverter
-from rpy2.robjects import pandas2ri
+from rpy2.robjects import r, pandas2ri
 
 PRINT_SUMMARY = False
 
@@ -47,7 +47,16 @@ def PropensityScoreMatchPsmPy(df, idColumn, columnsToMatch, sampleSize):
 
 
 def PropensityScoreMatchRMatchit(df, columnsToMatch, sampleSize):
-    matchit = importr('MatchIt')
+    try:
+        # Check if MatchIt is installed
+        importr('MatchIt')
+    except PackageNotInstalledError:
+        print("MatchIt is not installed. Installing now...")
+        # Install MatchIt
+        r('install.packages("MatchIt", repos="http://cran.r-project.org")')
+        # Verify installation
+        importr('MatchIt')
+        print("MatchIt installed successfully.")
     pandas2ri.activate()
     with localconverter(robjects.default_converter + pandas2ri.converter):
         r_data = robjects.conversion.py2rpy(df)
